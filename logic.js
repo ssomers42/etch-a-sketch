6ec7aa9df09833fbd5550fe0267860728c84a5a4
Stephan <ssomers42@gmail.com>
2022-03-30 21:50:34 -0700

    Refactored code

--- logic.js
+++ logic.js
@@ -1,70 +1,100 @@
 const CONTAINER = document.querySelector('#grid-container');
-const RESET = document.querySelector('#reset-btn');
+const RESIZE = document.querySelector('#resize-btn');
+const CLEAR = document.querySelector('#clear-btn');
 
 let gridSize = 16;
+let response = '';
+let itemList = [...document.querySelectorAll('.grid-item')]; //spread to transform nodelist into array to access .map
 
-//INPUT: Click reset button
-//OUTPUT: Select all items in grid. Remove color class. Prompt to resize grid. Call resizeGrid with new size.
+//resizes grid based on user input
+let resizeGrid = () => {
+  deleteGrid();
+  captureResponse();
+  createGrid();
+};
 
-let resetGrid = () => {
-  let itemList = document.querySelectorAll('.grid-item');
-  itemList = [...itemList]; //spread to transform nodelist into array to access .map
-  itemList.map(item => item.classList.remove('fill'));
-  response = prompt(
-    'How big would you like the grid? Please keep it less than 100.'
+//clear color from grid
+let clearGrid = () => {
+  deleteGrid();
+  createGrid();
+};
+
+//prompt when resizing grid, set response.
+let captureResponse = () => {
+  parseInt(
+    (response = prompt(
+      'How big would you like the grid? Please keep it less than 100.'
+    ))
   );
   if (response >= 100) {
-    resetGrid();
+    captureResponse();
   }
+};
+
+let deleteGrid = () => {
   let n = gridSize * gridSize;
   while (n > 0) {
     let gridItem = document.querySelector('.grid-item');
     CONTAINER.removeChild(gridItem);
     n--;
   }
-  gridSize = parseInt(response);
-  resizeGrid(gridSize);
 };
 
-let resizeGrid = gridSize => {
+let createGrid = () => {
   document.documentElement.style.setProperty(`--size`, `${gridSize}`);
-  console.log('resize');
-  console.log(gridSize);
   let n = gridSize * gridSize;
   while (n > 0) {
     const GRID = document.createElement('div');
     GRID.classList.add('grid-item');
     CONTAINER.appendChild(GRID);
-    GRID.addEventListener('mouseenter', event => {
-      let item = event.target;
-      let newColor = Math.floor(Math.random() * 255);
-      if (item.style.backgroundColor == '') {
-        let existingColor = `hsl(${newColor},100%, 50%)`;
-        item.style.backgroundColor = existingColor;
-      } else {
-        let digits = item.style.backgroundColor.match(/(\d+)/g);
-        let r = digits[0];
-        let g = digits[1];
-        let b = digits[2];
-
-        //convert rgb to hue
-        let hue = Math.round(
-          (Math.atan2(Math.sqrt(3) * (g - b), 2 * r - g - b) * 180) / Math.PI
-        );
-
-        //convert rgb to lightness
-        let lightness =
-          (((1 / 2) * (Math.max(r, g, b) + Math.min(r, g, b))) / 255) * 100 - 5;
-        console.log(lightness);
-
-        let darkenColor = `hsl(${hue}, 100%, ${lightness}%)`;
-        item.style.backgroundColor = darkenColor;
-      }
-    });
+    GRID.addEventListener('mouseenter', event => colorTiles(event));
     n--;
   }
 };
 
-RESET.addEventListener('click', resetGrid);
+//adds rainbow color to tiles.
+let colorTiles = event => {
+  let tileColor = event.target.style.backgroundColor;
 
-resizeGrid(16);
+  if (tileColor == '') {
+    setColor(event);
+  } else {
+    darkenColor(event);
+  }
+};
+
+let setColor = event => {
+  let randomHue = Math.floor(Math.random() * 255);
+  event.target.style.backgroundColor = `hsl(${randomHue},100%, 50%)`;
+};
+
+let darkenColor = event => {
+  let tileColor = event.target.style.backgroundColor;
+  let rgbInts = tileColor.match(/(\d+)/g); //RGB conversion
+  let r = rgbInts[0];
+  let g = rgbInts[1];
+  let b = rgbInts[2];
+
+  let hue = rgbToHue(r, g, b);
+  let lightness = rgbToLightness(r, g, b) - 5;
+  let darkenColor = `hsl(${hue}, 100%, ${lightness}%)`;
+  event.target.style.backgroundColor = darkenColor;
+};
+
+//convert rgb to ue
+let rgbToHue = (r, g, b) => {
+  return Math.round(
+    (Math.atan2(Math.sqrt(3) * (g - b), 2 * r - g - b) * 180) / Math.PI
+  );
+};
+
+//convert rgb to lightness
+let rgbToLightness = (r, g, b) => {
+  return (((1 / 2) * (Math.max(r, g, b) + Math.min(r, g, b))) / 255) * 100;
+};
+
+window.onload = () => {
+  createGrid(16);
+  RESIZE.addEventListener('click', resizeGrid);
+  CLEAR.addEventListener('click', clearGrid);
+};
